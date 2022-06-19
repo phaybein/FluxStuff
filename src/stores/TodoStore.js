95396b452d4e960519ce9d29bceada79eb5e03cd
Phaybein <fabian.m.martinez@gmail.com>
2022-06-19 07:54:27 -0700

    WIP: add actions method

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,6 +1,8 @@
 import {EventEmitter} from 'events';
 import {v4 as uuid} from 'uuid'
 
+import dispatcher from '../dispatcher';
+
 // EXTEND EVENTS EMITTER
 class TodoStore extends EventEmitter {
     constructor() {
@@ -19,11 +21,13 @@ class TodoStore extends EventEmitter {
         ]
     }
 
+    // GET ALL TODOS
     getAll()
     {
         return this.todos;
     }
 
+    // CREATE TODO
     createTodo(text) 
     {
         this.todos.push({
@@ -34,6 +38,19 @@ class TodoStore extends EventEmitter {
 
         this.emit("change");
     }
+
+    // HANDLE ALL ACTIONS
+    handleActions(action)
+    {
+        switch (action.type) {
+            case "CREATE_TODO":
+                this.createTodo(action.text);
+                break;
+        
+            default:
+                break;
+        }
+    }
 }
 
 // CREATE A NEW TODO STORE
@@ -41,6 +58,12 @@ const todoStore = new TodoStore;
 
 // !TESTING: TEMPORARILY EXPOSE TODO STORE
 // window.todoStore = todoStore;
+
+// REGISTER TODO STORE AND BIND THIS
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+
+// !TESTING: TEMPORARILY EXPOSE DISPATCHER
+window.dispatcher = dispatcher;
 
 // EXPORT TODO STORE
 export default todoStore;
