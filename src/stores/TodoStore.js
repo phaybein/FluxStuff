edce5edb7dd68e0659f55b6a0d11dddc3f6c2bd5
Phaybein <fabian.m.martinez@gmail.com>
2022-06-19 06:49:47 -0700

    WIP: create todo store

---
+++ src/stores/TodoStore.js
@@ -0,0 +1,12 @@
+import {EventEmitter} from 'events';
+
+// EXTEND EVENTS EMITTER
+class TodoStore extends EventEmitter {
+    
+}
+
+// CREATE A NEW TODO STORE
+const todoStore = new TodoStore;
+
+// EXPORT TODO STORE
+export default todoStore;
